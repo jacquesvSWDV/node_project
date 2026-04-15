afc98336d7b15c353b5198ae362813594d599017
jacquesvSWDV <jacques.valentine@rdpolytech.ca>
2026-04-15 10:51:10 -0600

    Upd

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const rateLimit = require("express-rate-limit");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
+const cache = require("apicache").middleware;
 
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
@@ -42,6 +43,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(throttling);
 app.use(limiter);
+app.use(cache("5 minutes"));
 app.use(userRoutes);
 app.use(authRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
